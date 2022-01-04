0eb4b09e2fa180d1187a347abfa17a763fa72786
busesrcglu <serceoglubuse@gmail.com>
2022-01-04 15:59:14 +0300

    refactor: yield curve calculation edited

--- yieldcurve/js/src/index.js
+++ yieldcurve/js/src/index.js
@@ -60,15 +60,9 @@ function createTaskResult() {
                 }else {
                     taskData["yieldCurve"] = yieldCurveObject
                 }
-
             }
             else if (data.name.includes("yieldData")) {
-                if (data.value) {
-                    isJSONObject(data.value, "yieldData")
-                }else {
-                    taskData["yieldData"] = yieldDataObject
-                }
-
+                isJSONObject(data.value ? data.value : JSON.stringify(yieldDataObject), "yieldData")
             }
             else {
                     taskData[data.name] = data.value
@@ -76,81 +70,79 @@ function createTaskResult() {
         });
         let client = new rhoova.RhoovaClient({apiKey: apiKey, apiSecret: apiSecret});
 		client.createTask({data: taskData, calculationType: rhoova.CalculationType.YIELD_CURVE, waitResult: true}).then((result) => {
-            if(result.error){
-                console.log(result.error)
+            document.getElementById("zeroResultBody").innerHTML = "";
+            document.getElementById("forwardResultBody").innerHTML = "";
+            document.getElementById("discountResultBody").innerHTML = "";
+            document.getElementById("loadingIcon").style.display = "none";
+		    if(result.error){
+                document.getElementById("taskPropertiesResult").style.display = "none";
+                document.getElementById("errorMessage").innerHTML = "";
+                document.getElementById("errorResult").style.display = "block";
+                document.getElementById("errorMessage").innerHTML = JSON.stringify((result.error));
             }else{
                 document.getElementById("loadingIcon").style.display = "none";
                 let resultData = JSON.parse(result.result);
-                console.log("result", resultData);
                 let yValues = [];
                 let xValues  = [];
                 resultData.zeroRates.forEach((data)=> {
                     yValues.push(data.rate);
                     xValues.push(data.date);
+                    let tBody = '<tr><td>'+data.date+'</td><td>'+ data.rate+'</td></tr>';
+                    $('#zeroResultBody').append(tBody);
                 });
-                var myBarChart = new Chart(document.getElementById("bar-chart"), {
+                resultData.forwardRates.forEach((data)=> {
+                    let tBody = '<tr><td>'+data.date+'</td><td>'+ data.rate+'</td></tr>';
+                    $('#forwardResultBody').append(tBody);
+                });
+                resultData.discountRates.forEach((data)=> {
+                    let tBody = '<tr><td>'+data.date+'</td><td>'+ data.rate+'</td></tr>';
+                    $('#discountResultBody').append(tBody);
+                });
+                resultData.yieldNodeData.forEach((data)=> {
+                    let tBody = '<tr><td>'+data.date+'</td><td>'+ data.rate+'</td></tr>';
+                    $('#nodeDataResultBody').append(tBody);
+                });
+
+                let kanvas = document.getElementById('line-chart');
+                let grafik = new Chart(kanvas, {
                     type: 'line',
                     data: {
-                        labels: valuelabel,
-                        datasets: [
-                            {
-                                label: "Population (millions)",
-                                backgroundColor: "#3e95cd",
-                                data: valuedata,
-
-                            }
-                        ]
+                        labels: xValues,
+                        datasets: [{
+                            label: 'Zero Rates',
+                            data: yValues,
+                            borderColor: [
+                                'rgba(255, 100, 10, 120)'
+                            ],
+                            fill: false,
+                            borderWidth: 1
+                        }]
                     },
                     options: {
-                        legend: { display: false },
-                        title: {
-                            display: true,
-                            text: 'Predicted world population (millions) in 2050'
-                        },
                         scales: {
                             xAxes: [{
-                                gridLines: {
-                                    display:false
-                                }
+                                ticks: {
+                                    fontColor: "#CCC", // this here
+                                },
                             }],
                             yAxes: [{
-                                gridLines: {
-                                    display:false,
-                                    drawBorder: false
-                                },
                                 ticks: {
-                                    display: false
-                                }
-                            }]
-                        },
-                        plugins: {
-                            datalabels: {
-                                color: 'white',
-                                display: function(context) {
-                                    console.log("Algo: "+context);
-                                    return context.dataset.data[context.dataIndex] > 15;
+                                    fontColor: "#CCC", // this here
                                 },
-                                font: {
-                                    weight: 'bold'
-                                },
-                                formatter: function(value, context) {
-                                    return context.dataIndex + ': ' + Math.round(value*100) + '%';
-                                }
-                            }
+                            }],
                         }
                     }
                 });
-            //    let ctx = document.getElementById('myChart').getContext(2);
 
-                console.log(document.getElementById('bar-chart').value);
-             //   document.getElementById('mycanvas').data2.value= yValues;
-
+                document.getElementById("taskPropertiesResult").style.display = "block";
+                document.getElementById("errorResult").style.display = "none"
 
             }
 
         }).catch(error => {
             document.getElementById("loadingIcon").style.display = "none";
             document.getElementById("errorMessage").innerHTML = "";
+            document.getElementById("taskPropertiesResult").style.display = "none";
             document.getElementById("errorResult").style.display = "block";
             document.getElementById("errorMessage").innerHTML = JSON.stringify((error));
             console.log(error)
@@ -160,17 +152,19 @@ function createTaskResult() {
 
 function isJSONObject(data, label) {
     try {
+        JSON.parse(data);
         if (document.activeElement.id === "randomizeAndSubmit" && label === "yieldData") {
             taskData[label] = randomizeData(JSON.parse(data))
         } else if (document.activeElement.id === "submit" && label === "yieldData") {
             taskData[label] = JSON.parse(data)
-        } if (label === 'startEndDates'){
+        } else if (label === 'startEndDates'){
             taskData["forwardRates"][label] = JSON.parse(data)
         } else {
             taskData[label] = JSON.parse(data)
         }
     } catch (e) {
         document.getElementById("loadingIcon").style.display = "none";
+        document.getElementById("taskPropertiesResult").style.display = "none";
         document.getElementById("errorMessage").innerHTML = "";
         document.getElementById("errorResult").style.display = "block";
         document.getElementById("errorMessage").innerHTML = "Invalid "+ label + " object";
@@ -183,6 +177,7 @@ function randomizeData(data){
     data.forEach((item, index) => {
         item.value = Math.floor((Math.random() * item.value+0.01) - item.value-0.01) *  0.0025;
         data[index] = item;
+
     });
 
     return data;
